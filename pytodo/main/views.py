943e39790a1463af1608bd5595418d89ad1c7a89
Mark Vasilkov <mvasilkov@gmail.com>
2012-04-20 13:45:09 +0300

    del_task

--- pytodo/main/views.py
+++ pytodo/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from pytodo.main.forms import TaskForm
 from pytodo.main.models import Task
 
@@ -14,3 +14,9 @@ def new_task(request):
         return redirect('home')
 
     return render(request, 'new_task.html', {'form': form})
+
+
+def del_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    task.delete()
+    return redirect('home')
